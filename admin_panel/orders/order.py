fd58ba659e4cbb3367f9d1d6932bb4b7bb3b8af0
KOMILJONOV <komiljonovshukurullox@gmail.com>
2022-06-25 19:08:08 +0500

    dfgdfg

--- admin_panel/orders/order.py
+++ admin_panel/orders/order.py
@@ -554,7 +554,7 @@ def makeorderpdf(request, order):
         50 + (10 * i),
         xxx[i]
     )
-        
+    fpdf.PY3K = False
 
     # buffer = file.output(f'order_{str(order.id)}.pdf'.encode('latin-1', 'replace').decode(), "S")
     buffer = file.output(dest='S')
